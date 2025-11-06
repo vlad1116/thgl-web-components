a012deab5555d595d28360d93fe1ad2ade1edb58
lmachens <leon@machens.koeln>
2025-11-06 12:17:10 +0100

    Ad DNA config

--- apps/duet-night-abyss-web/src/config.ts
+++ apps/duet-night-abyss-web/src/config.ts
@@ -8,13 +8,79 @@ export const APP_CONFIG: AppConfig = {
   appUrl: "https://www.th.gl/companion-app",
   internalLinks: [
     {
+      title: "Purgatorio Island Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Purgatorio Island with our interactive maps.",
+      href: "/maps/Purgatorio%20Island",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/duet-night-abyss/map-tiles/Prologue/preview.webp",
+      linkText: "Explore the Purgatorio Island Map",
+    },
+    {
+      title: "Lonza Fortress Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Lonza Fortress with our interactive maps.",
+      href: "/maps/Lonza%20Fortress",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/duet-night-abyss/map-tiles/EX01/preview.webp",
+      linkText: "Explore the Lonza Fortress Map",
+    },
+    {
+      title: "Eastern District, Icelake Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Eastern District, Icelake with our interactive maps.",
+      href: "/maps/Eastern%20District%2C%20Icelake",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/duet-night-abyss/map-tiles/Chapter01/preview.webp",
+      linkText: "Explore the Eastern District, Icelake Map",
+    },
+    {
+      title: "Glevum Pit Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Glevum Pit with our interactive maps.",
+      href: "/maps/Glevum%20Pit",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/duet-night-abyss/map-tiles/Chapter01_KK/preview.webp",
+      linkText: "Explore the Glevum Pit Map",
+    },
+    {
+      title: "Icelake Sewers Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Icelake Sewers with our interactive maps.",
+      href: "/maps/Icelake%20Sewers",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/duet-night-abyss/map-tiles/Chapter01_Sew/preview.webp",
+      linkText: "Explore the Icelake Sewers Map",
+    },
+    {
+      title: "Galea Theatre Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Galea Theatre with our interactive maps.",
+      href: "/maps/Galea%20Theatre",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/duet-night-abyss/map-tiles/Chapter01_Thea/preview.webp",
+      linkText: "Explore the Galea Theatre Map",
+    },
+    {
       href: "/guides",
       title: "config.internalLinks.guides.title",
       linkText: "config.internalLinks.guides.linkText",
       iconName: "BookOpen",
     },
+    {
+      href: "/guides/Geniemon%20(Pets)",
+      title: "Geniemon (Pets)",
+      linkText: "Discover all Geniemon (Pets)",
+      iconName: "BookOpen",
+    },
   ],
   promoLinks: [],
   externalLinks: [],
-  keywords: [],
+  keywords: ["DNA", "Geniemon", "Chests", "Collectibles", "Readables"],
 };
