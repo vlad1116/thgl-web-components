01c47db5613c258be4ba3016626307b6ae7f0972
lmachens <leon@machens.koeln>
2025-11-18 13:07:35 +0100

    Temporary remove btloader check because of cloudflare  issues

--- packages/ui/src/components/(ads)/nitro-script.tsx
+++ packages/ui/src/components/(ads)/nitro-script.tsx
@@ -274,17 +274,17 @@ export function ScriptLoader({
                 { once: true },
               );
             }
-          } else if (node.nodeName === "SCRIPT") {
-            const script = node as HTMLScriptElement;
-            if (script.src.includes("btloader.com")) {
-              script.addEventListener(
-                "error",
-                () => {
-                  setState(STATE_ERROR);
-                },
-                { once: true },
-              );
-            }
+            // } else if (node.nodeName === "SCRIPT") {
+            //   const script = node as HTMLScriptElement;
+            //   if (script.src.includes("btloader.com")) {
+            //     script.addEventListener(
+            //       "error",
+            //       (e) => {
+            //         setState(STATE_ERROR);
+            //       },
+            //       { once: true },
+            //     );
+            //   }
           }
         });
       });
